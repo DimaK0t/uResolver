2eea620e964bb2a3d43a40afe896a84ee6696b76
Dmitriy Martyscenko <mart.dmitriy@gmail.com>
2015-04-08 13:24:28 +0300

    Refacrored.

---
+++ uPackageResolver/Packages/PackageManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using uPackageResolver.Models;
+
+namespace uPackageResolver.Packages
+{
+    public class PackageManager
+    {
+        public void PlaceFileToPackageDirectory(string from, string to, string fileName)
+        {
+            if (!Directory.Exists(from))
+            {
+                Directory.CreateDirectory(from);
+            }
+
+            var destFile = Path.Combine(from, fileName);
+            if (!File.Exists(destFile))
+            {
+                File.Copy(to, destFile);
+            }
+        }
+
+        public IEnumerable<FileModel> GetPackageFiles(string packageManifestPath)
+        {
+            var manifest = XDocument.Load(packageManifestPath);
+            var files = manifest.Root.Element("files").Elements("file");
+            return files.Select(x => new FileModel()
+            {
+                FileGuid = x.Element("guid").Value,
+                OrgName = x.Element("orgName").Value,
+                OrgPath = x.Element("orgPath").Value
+            });
+        }
+
+        public IEnumerable<PackageModel> GetInstalledPackages(string installedPackageConfigPath)
+        {
+            var config = XDocument.Load(installedPackageConfigPath);
+            var packages = config.Elements("packages").Elements("package").Select(x => new PackageModel
+            {
+                RepoGuid = x.Attribute("repositoryGuid").Value,
+                PackageGuid = x.Attribute("packageGuid").Value
+            }).ToList();
+
+            return packages.Where(x => !string.IsNullOrEmpty(x.PackageGuid) && !string.IsNullOrEmpty(x.RepoGuid));
+        }
+    }
+}
--- uPackageResolver/Program.cs
+++ uPackageResolver/Program.cs
@@ -10,24 +10,27 @@ using Fclp;
 using Fclp.Internals.Extensions;
 using uPackageResolver.Login;
 using uPackageResolver.Models;
+using uPackageResolver.Packages;
 
 namespace uPackageResolver
 {
     internal class Program
     {
-        private static AuthManager _authManager = new AuthManager();
+        private static readonly AuthManager _authManager = new AuthManager();
+        private static readonly PackageManager _packageManager = new PackageManager();
         private static readonly string _basePath = Environment.CurrentDirectory;
         private const string _appDataFolder = @"\App_Data\";
 
         private static void Main(string[] args)
         {
             var options = SetupArguments(args);
-            if (options == null || options.Host.IsNullOrEmpty() || options.UserName.IsNullOrEmpty() || options.Password.IsNullOrEmpty())
+            if (options == null || options.Host.IsNullOrEmpty() || options.UserName.IsNullOrEmpty() ||
+                options.Password.IsNullOrEmpty())
             {
                 return;
             }
 
-            RestorePackages(options.Host, options.UserName, options.Password).Wait();
+            RestorePackages(options.Host, options.UserName, options.Password);
         }
 
         private static Args SetupArguments(string[] args)
@@ -36,7 +39,10 @@ namespace uPackageResolver
             parser.Setup(x => x.Host).As('h').Required().WithDescription("Host name of your site. Requaired");
             parser.Setup(x => x.UserName).As('u').Required().WithDescription("Umbraco`s user name. Requaired");
             parser.Setup(x => x.Password).As('p').Required().WithDescription("Umraco`s password.  Requaired");
-            parser.SetupHelp("?", "help").UseForEmptyArgs().WithHeader("Example: uresolver -h site.com -u admin -p password").Callback(text => Console.WriteLine(text));
+            parser.SetupHelp("?", "help")
+                .UseForEmptyArgs()
+                .WithHeader("Example: uresolver -h site.com -u admin -p password")
+                .Callback(text => Console.WriteLine(text));
 
             var options = parser.Parse(args);
             if (options.HasErrors)
@@ -53,15 +59,17 @@ namespace uPackageResolver
         {
             var packagePath = Path.Combine(_basePath + _appDataFolder, packageModel.PackageGuid);
             var packageParams = new Dictionary<string, string>
-                    {
-                        {"body_tempFile", packagePath },
-                    };
-            var downloadPackageUrl = string.Format("{2}/umbraco/developer/packages/installer.aspx?repoGuid={0}&guid={1}", packageModel.RepoGuid, packageModel.PackageGuid, host);
+            {
+                {"body_tempFile", packagePath},
+            };
+            var downloadPackageUrl = string.Format(
+                "{2}/umbraco/developer/packages/installer.aspx?repoGuid={0}&guid={1}", packageModel.RepoGuid,
+                packageModel.PackageGuid, host);
             var response = await client.PostAsync(downloadPackageUrl, new FormUrlEncodedContent(packageParams));
             return response;
         }
 
-        private static async Task RestorePackages(string host, string userName, string password)
+        private static async void RestorePackages(string host, string userName, string password)
         {
             try
             {
@@ -77,7 +85,9 @@ namespace uPackageResolver
                     Console.WriteLine("Respons.StatusCode: " + loginResp.StatusCode);
                     Console.WriteLine("Loged in");
 
-                    var packages = GetPackages();
+                    var installedPackagesConfigPath = Path.Combine(_basePath + _appDataFolder,
+                        @"packages\installed\installedPackages.config");
+                    var packages = _packageManager.GetInstalledPackages(installedPackagesConfigPath);
                     Console.WriteLine("Found {0} packages ", packages.Count());
                     foreach (var packageModel in packages)
                     {
@@ -93,10 +103,14 @@ namespace uPackageResolver
                                 Console.WriteLine("Downloaded");
 
                                 // copy files to final destinations
-                                foreach (var file in GetFiles(model.PackageGuid))
+                                var manifestPath = Path.Combine(_basePath + _appDataFolder, model.PackageGuid,
+                                    @"package.xml");
+
+                                foreach (var file in _packageManager.GetPackageFiles(manifestPath))
                                 {
-                                    var soursePath = Path.Combine(_basePath + _appDataFolder, model.PackageGuid, file.FileGuid);
-                                    PlaceFileToPackageDirectory(soursePath, file);
+                                    var from = _basePath + file.OrgPath;
+                                    var to = Path.Combine(_basePath + _appDataFolder, model.PackageGuid, file.FileGuid);
+                                    _packageManager.PlaceFileToPackageDirectory(from, to, file.OrgName);
                                 }
                             }
                             catch (Exception e)
@@ -121,45 +135,6 @@ namespace uPackageResolver
             Console.WriteLine(exception.InnerException);
             Console.ForegroundColor = ConsoleColor.White;
             Environment.Exit(Environment.ExitCode);
-        }
-
-        private static void PlaceFileToPackageDirectory(string soursePath, FileModel file)
-        {
-            var destDirectory = _basePath + file.OrgPath;
-            if (!Directory.Exists(destDirectory))
-            {
-                Directory.CreateDirectory(destDirectory);
-            }
-
-            var destFile = Path.Combine(destDirectory, file.OrgName);
-            if (!File.Exists(destFile))
-            {
-                File.Copy(soursePath, destFile);
-            }
-        }
-
-        private static IEnumerable<FileModel> GetFiles(string packageGuid)
-        {
-            var manifest = XDocument.Load(Path.Combine(_basePath + _appDataFolder, packageGuid, @"package.xml"));
-            var files = manifest.Root.Element("files").Elements("file");
-            return files.Select(x => new FileModel()
-            {
-                FileGuid = x.Element("guid").Value,
-                OrgName = x.Element("orgName").Value,
-                OrgPath = x.Element("orgPath").Value
-            });
-        }
-
-        private static IEnumerable<PackageModel> GetPackages()
-        {
-            var config = XDocument.Load(Path.Combine(_basePath + _appDataFolder, @"packages\installed\installedPackages.config"));
-            var packages = config.Elements("packages").Elements("package").Select(x => new PackageModel
-            {
-                RepoGuid = x.Attribute("repositoryGuid").Value,
-                PackageGuid = x.Attribute("packageGuid").Value
-            }).ToList();
-
-            return packages.Where(x => !string.IsNullOrEmpty(x.PackageGuid) && !string.IsNullOrEmpty(x.RepoGuid));
         }
     }
 }
